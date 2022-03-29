94de5d860cb62f7dadff56d392d73550d5f46574
binghe <1028386804@qq.com>
2022-03-29 15:44:54 +0800

    update github address

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/MaLunan/malunan.github.io.git master
+git push -f https://github.com/binghe002/binghe002.github.io.git master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
