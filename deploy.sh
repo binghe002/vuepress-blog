0b5d0aebb3478a8060792f05ec4f4bb65e2ed8fb
binghe <1028386804@qq.com>
2022-03-29 18:31:39 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 更新文章
-git add .
-git commit -m '更新文章'
-git push
-
-# 生成静态文件
+## 更新文章
+#git add .
+#git commit -m '更新文章'
+#git push
+#
+## 生成静态文件
+rm -rf .site
 npm install
 npm run docs:build
 
@@ -18,7 +19,7 @@ cd .site
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
+#git init
 git add -A
 git commit -m 'deploy'
 
