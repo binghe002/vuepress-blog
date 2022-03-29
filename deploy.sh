1f799d2097f31dc84d4418ff2ab273528d02b887
binghe <1028386804@qq.com>
2022-03-29 17:07:49 +0800

    更新文章

--- deploy.sh
+++ deploy.sh
@@ -3,34 +3,27 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm install
-npm run docs:build
-
-# 进入生成的文件夹
-mv docs/.vuepress/dist/* blog/
-
 # 更新文章
 git add .
 git commit -m '更新文章'
 git push
 
 # 生成静态文件
-#npm install
-#npm run docs:build
-#
-## 进入生成的文件夹
-#cd docs/.vuepress/dist
+npm install
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-#git init
-#git add -A
-#git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f https://binghe002@github.com/binghe002/binghe002.github.io.git master
+git push -f https://binghe002@github.com/binghe002/binghe002.github.io.git master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
